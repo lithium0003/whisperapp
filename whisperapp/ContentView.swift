7eb023dbc5dd889aa79f0ce17cb0f704a0f479c9
lithium03 <contact@lithium03.info>
2024-04-25 19:19:20 +0900

    Initial Commit

---
+++ whisperapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whisperapp
+//
+//  Created by rei9 on 2024/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whisperapp/whisperappApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisperappApp.swift
+//  whisperapp
+//
+//  Created by rei9 on 2024/04/25.
+//
+
+import SwiftUI
+
+@main
+struct whisperappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
